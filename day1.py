3e9d9fdc851e2db18f370dd9c42e7d8410cb613f
Parthasarathy <parthasarathy1718333@gmail.com>
2026-02-05 10:08:53 +0530

    Initial commit - Day 1

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("hello,this is my day1 for python coding challenge for gen ai")
+print("Parthasarathy P")#name
+print("I am learning python programming language")#task
+print("I am excited to be part of this challenge!")#excitement
